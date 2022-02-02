f91d3d7ad55a1a9373a692eaf4e47b5af8f34c5d
turosung <82477640+turosung@users.noreply.github.com>
2022-02-02 01:31:11 +0000

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
 // Select color input
+const cellColor = document.getElementById('colorPicker');
+
 // Select size input
+const gridSize = document.getElementById('sizePicker');
+const gridHeight = document.getElementById('inputHeight');
+const gridWidth = document.getElementById('inputWidth');
+const submitButton = document.getElementById('submit');
+const canvas = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+gridSize.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const gridHeight = document.getElementById('inputHeight').value;
+    const gridWidth = document.getElementById('inputWidth').value;
+    makeGrid(gridHeight, gridWidth);
+    // console.log('Grid Height: ' + gridHeight + 'Grid Width: ' + gridWidth); 
+});
 
+function makeGrid(gridHeight, gridWidth) {
+    canvas.innerHTML = "";
+    for(let i = 0; i < gridHeight; i++) {
+        let row = document.createElement('tr');
+        for (let x = 0; x < gridWidth; x++ ); {
+             let cell = document.createElement('td');
+             row.appendChild(cell);
+        }
+         canvas.appendChild(row); 
+    }
+};
 // Your code goes here!
-
-}
